70ee3959509cc0a2f722712d05075365cb221c86
redberry0217 <redberry0217@gmail.com>
2025-02-13 17:23:56 +0900

    feat: Header 컴포넌트 틀 잡기

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
-    <div className="border-b border-[#eeccff] bg-[#000000] flex flex-col gap-[10px]">
-      <p>ㅇㅇㅇ</p>
-      <p>ㅅㅅㅅ</p>
+    <div className="h-20 border-b border-gray-300 flex gap-10 items-center justify-center">
+      <div className="w-[1400px] flex items-center justify-center bg-blue-100">
+        {/* 가운데 페이지 메뉴 섹션 */}
+        <section className="w-[90%] flex items-center justify-center">
+          <Link to="/" className="text-center">
+            메인
+          </Link>
+        </section>
+        {/* 오른쪽 로그인 메뉴 섹션 */}
+        <section className="w-[10%] flex items-center justify-center">
+          <Link to="/login" className="text-right">
+            로그인
+          </Link>
+        </section>
+      </div>
     </div>
   );
 };
