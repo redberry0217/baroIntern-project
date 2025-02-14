604ee7aaf39057631ac7b2691bd070b257ddd56a
redberry0217 <redberry0217@gmail.com>
2025-02-14 22:23:32 +0900

    feat: 로그인 상태에 따른 Header 조건부렌더링

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Button from "./ui/Button";
 
 const Header = () => {
+  const isLoggedIn = localStorage.getItem("accessToken") !== null;
+  const navigate = useNavigate();
+
+  /** functions */
+  // 로그아웃
+  const handleLogout = () => {
+    localStorage.removeItem("accessToken");
+    alert("로그아웃 되었습니다.");
+    navigate("/");
+  };
+
   return (
     <div className="h-20 border-b border-gray-300 flex gap-10 items-center justify-center bg-blue-100">
       <div className="w-[1000px] flex items-center">
@@ -10,9 +22,30 @@ const Header = () => {
         <Link to="/" className="w-1/3 text-center">
           <strong>메인페이지</strong>
         </Link>
-        <Link to="/login" className="w-1/3 text-right">
-          <strong>로그인</strong>
-        </Link>
+        {isLoggedIn ? (
+          <div className="w-1/3 flex gap-6 justify-end">
+            <Button
+              onClick={() => navigate("/mypage")}
+              color="default"
+              width="w-[120px]"
+            >
+              <strong>마이페이지</strong>
+            </Button>
+            <Button onClick={handleLogout} color="primary" width="w-[120px]">
+              <strong>로그아웃</strong>
+            </Button>
+          </div>
+        ) : (
+          <div className="w-1/3 flex justify-end">
+            <Button
+              onClick={() => navigate("/login")}
+              color="default"
+              width="w-[120px]"
+            >
+              <strong>로그인</strong>
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
