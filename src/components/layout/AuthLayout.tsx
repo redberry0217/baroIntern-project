90a98ebdb5f440450560394b2acf99ccffc8fbe1
redberry0217 <redberry0217@gmail.com>
2025-02-16 22:03:18 +0900

    chore: 주석 처리 등 소소한 수정

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header";
-// import useAuthRedirect from "../../hooks/useRedirect";
+import useAuthRedirect from "../../hooks/useRedirect";
 
 const AuthLayout = () => {
-  // const { isUserLoading } = useAuthRedirect();
+  const { isUserLoading } = useAuthRedirect();
 
-  // if (isUserLoading) {
-  //   return <div>Loading...</div>;
-  // }
+  if (isUserLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
